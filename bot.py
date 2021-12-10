f1a81105211aad69ac8a00ef4543dbdf6eae9f7c
Khakim <khakim.zhumagaliyev@mail.ru>
2021-12-10 22:54:00 +0600

    some changes

--- bot.py
+++ bot.py
@@ -9,26 +9,42 @@ bot = telebot.TeleBot(config.TOKEN)
 def welcome(message):
     sti = open('static/sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, sti)
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n Я - <b>{1.first_name}</b>, бот созданный для того чтобы помогать студентам СДУ.".format(message.from_user, bot.get_me()),
+                     parse_mode='html')
 
+@bot.message_handler(commands=['get_info', 'info'])
+def get_info(message):
+    markup = types.InlineKeyboardMarkup()
+    option_1 = types.InlineKeyboardButton("Faculties", callback_data = 'faculties')
+    option_2 = types.InlineKeyboardButton("Other", callback_data = 'others')
+    markup.add(option_1, option_2)
+    bot.send_message(message.chat.id, "Choose between two", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'faculties')
+def callback_faculties(call):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Do Smth 1")
-    item2 = types.KeyboardButton("Do Smth 2")
-    markup.add(item1, item2)
+    option_1 = types.KeyboardButton("Engenieer")
+    option_2 = types.KeyboardButton("Law")
+    option_3 = types.KeyboardButton("Medical")
+    markup.add(option_1, option_2, option_3)
+    bot.send_message(call.message.chat.id,'Choose your faculty', reply_markup=markup)
 
-    bot.send_message(message.chat.id, "Welcome to the chat, {0.first_name}!\n I am - <b>{1.first_name}</b>, created to help you organize your day.".format(message.from_user, bot.get_me()),
-                     parse_mode='html', reply_markup=markup)
+@bot.callback_query_handler(func=lambda call: call.data == 'others')
+def callback_others(call):
+    pass
+
+
 
 
 @bot.message_handler(content_types=['text'])
 def m_bot(message):
     # bot.send_message(message.chat.id, message.text)
-    if message.chat.type == 'private':
-        if message.text == "Do Smth 1":
-            bot.send_message(message.chat.id, "I am done")
-        elif message.text == "Do Smth 2":
-            bot.send_message(message.chat.id, "How r u")
-        else:
-            bot.send_message(message.chat.id, "I do not knoww")
+    if message.text == 'Engenieer':
+        bot.send_message(message.chat.id, "Your faculty is Engineer")
+    elif message.text == 'Law':
+        bot.send_message(message.chat.id, "Your faculty is Law")
+    elif message.text == 'Medical':
+        bot.send_message(message.chat.id, "Your faculty is Medical")
 
 
 bot.polling(none_stop=True)
